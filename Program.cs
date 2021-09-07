265647f00a10052d1928fb78c204e9e61800e0d9
Eleanor Miller <eleanormiller.dev@gmail.com>
2021-09-07 18:44:06 -0400

    Adds the ability to *READ* the list of numbers

--- Program.cs
+++ Program.cs
@@ -14,65 +14,58 @@ namespace NumberTracker
         {
             Console.WriteLine("Welcome to Number Tracker");
 
-            //var fileReader = new StreamReader("numbers.csv");
-            // Generic reader that can be a StreamReader *OR* a String Reader
-            TextReader reader;
+            var numbers = new List<int>();
 
             // If the file exists
             if (File.Exists("numbers.csv"))
             {
-                // Assign a StreamReader to read from the file
-                reader = new StreamReader("numbers.csv");
-            }
-            else
-            {
-                // Assign a StringReader to read from an empty string
-                reader = new StringReader("");
-            }
+                // Create a file reader to read from numbers.csv
+                var fileReader = new StreamReader("numbers.csv");
 
-            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
-            {
-                HasHeaderRecord = false,
-            };
-
-            var csvReader = new CsvReader(reader, config);
-
-            var numbers = csvReader.GetRecords<int>().ToList();
-
-            var isRunning = true;
-
-            while (isRunning)
-            {
-                Console.WriteLine("------------------");
-                foreach (var number in numbers)
+                var config = new CsvConfiguration(CultureInfo.InvariantCulture)
                 {
-                    Console.WriteLine(number);
-                }
-                Console.WriteLine($"Our list has: {numbers.Count()} entries");
-                Console.WriteLine("------------------");
-                Console.Write("Enter a number to store, or 'quit' to end: ");
-                var input = Console.ReadLine().ToLower();
+                    HasHeaderRecord = false,
+                };
 
-                if (input == "quit")
+                var csvReader = new CsvReader(fileReader, config);
+
+                numbers = csvReader.GetRecords<int>().ToList();
+
+                var isRunning = true;
+
+                while (isRunning)
                 {
-                    isRunning = false;
+                    Console.WriteLine("------------------");
+                    foreach (var number in numbers)
+                    {
+                        Console.WriteLine(number);
+                    }
+                    Console.WriteLine($"Our list has: {numbers.Count()} entries");
+                    Console.WriteLine("------------------");
+                    Console.Write("Enter a number to store, or 'quit' to end: ");
+                    var input = Console.ReadLine().ToLower();
+
+                    if (input == "quit")
+                    {
+                        isRunning = false;
+                    }
+                    else
+                    {
+                        var number = int.Parse(input);
+                        numbers.Add(number);
+                    }
                 }
-                else
-                {
-                    var number = int.Parse(input);
-                    numbers.Add(number);
-                }
+
+                var fileWriter = new StreamWriter("numbers.csv");
+
+                var csvWriter = new CsvWriter(fileWriter, CultureInfo.InvariantCulture);
+
+                csvWriter.WriteRecords(numbers);
+
+                fileWriter.Close();
+
+
             }
-
-            var fileWriter = new StreamWriter("numbers.csv");
-
-            var csvWriter = new CsvWriter(fileWriter, CultureInfo.InvariantCulture);
-
-            csvWriter.WriteRecords(numbers);
-
-            fileWriter.Close();
-
-
         }
     }
 }
